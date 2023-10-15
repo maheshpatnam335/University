2f92413f725ab258fde986f0e9f8711b5fe18eb7
maheshpatnam335 <maheshpatnam335@gmail.com>
2023-10-15 14:13:40 +0530

    added Entities/TimeSheet

---
+++ server/EntitiesAndModels/Entities/TimeSheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesAndModels.Entities
+{
+    public class TimeSheet
+    {
+        public int Id { get; set; }
+        public int LogId { get; set; }
+        public DateTime PunchIn { get; set; }
+        public DateTime PunchOut { get; set; }
+    }
+}
---
+++ server/EntitiesAndModels/Models/Authorisation/TimesheetModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesAndModels.Models.Authorisation
+{
+    public class TimesheetModel
+    {
+            public int IsPunchInOrOut { get; set; }
+            public int LogId { get; set; }
+    }
+}
